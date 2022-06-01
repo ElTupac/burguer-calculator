db9b31f399ff9450b2d6f63ba6496b13fcb57232
ElTupac <62774311+ElTupac@users.noreply.github.com>
2022-06-01 20:53:46 -0300

    Wrote burguer rules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ingredients = {
-  vetegables: [
+  vegetable: [
     "tomato",
     "onion",
     "lettuce",
@@ -7,36 +7,78 @@ const ingredients = {
     "mushrooms",
     "bellpepper",
   ],
-  proteins: ["beef", "chicken", "pork", "lentils", "chickpeas"],
-  cheeses: ["cheddar", "gouda", "parmesan", "mozzarella"],
-  breads: ["potato bread", "white bread", "sesame bread"],
+  protein: ["beef", "chicken", "pork", "lentils", "chickpeas"],
+  cheese: ["cheddar", "gouda", "parmesan", "mozzarella"],
+  bread: ["potato bread", "white bread", "sesame bread"],
+  sauce: ["mayonnaise", "ketchup", "mustard", "barbaque", "chiplotle", "spicy"],
 };
+
+const isWhatIngredient = (ingredient) => {
+  for (const possibility in ingredients) {
+    if (
+      ingredients[possibility].some(
+        (possible_ingredient) => possible_ingredient === ingredient
+      )
+    )
+      return possibility;
+  }
+  return null;
+};
+
+const isThisIngredient = (ingredient, guess) =>
+  ingredients[guess].some((possibility) => possibility === ingredient);
 
 const rules = [];
 
 rules.push((burguer) => {
-  //Tiene pan arriba y abajo
-  return true || false;
+  const validation =
+    isThisIngredient(burguer[0], "bread") &&
+    isThisIngredient(burguer[burguer.length - 1], "bread");
+  console.log("Tiene pan arriba y abajo: " + validation);
+  return validation;
 });
 
 rules.push((burguer) => {
-  //Tiene al menos dos verduras
-  return true || false;
+  let vegetableCount = 0;
+  burguer.map((ingredient) => {
+    if (isThisIngredient(ingredient, "vegetable")) vegetableCount++;
+  });
+  const validation = vegetableCount >= 2;
+  console.log("Tiene al menos dos verduras: " + validation);
+  return validation;
 });
 
 rules.push((burguer) => {
-  //Tiene 1 sola salsa
-  return true || false;
+  let sauceCount = 0;
+  burguer.map((ingredient) => {
+    if (isThisIngredient(ingredient, "sauce")) sauceCount++;
+  });
+  const validation = sauceCount === 1;
+  console.log("Tiene 1 sola salsa: " + validation);
+  return validation;
 });
 
 rules.push((burguer) => {
-  //Tiene 2 o menos proteinas
-  return true || false;
+  let proteinCount = 0;
+  burguer.map((ingredient) => {
+    if (isThisIngredient(ingredient, "protein")) proteinCount++;
+  });
+  const validation = proteinCount <= 2;
+  console.log("Tiene 2 o menos proteinas: " + validation);
+  return validation;
 });
 
 rules.push((burguer) => {
-  //Tiene queso en la posicion anterior a cualquier proteina
-  return false;
+  const validation = !burguer.some((ingredient, index) => {
+    if (isThisIngredient(ingredient, "protein")) {
+      if (index === 0) return true;
+      return !isThisIngredient(burguer[index - 1], "cheese");
+    }
+  });
+  console.log(
+    "Tiene queso en la posicion anterior a cualquier proteina: " + validation
+  );
+  return validation;
 });
 
 class Burguer {
@@ -53,11 +95,21 @@ class Burguer {
   static calculateScore(burguer) {
     let score = 5;
     rules.map((func) => {
-      if (!func(burguer["ingredients"])) score -= 1;
+      if (!func(burguer)) score -= 1;
     });
     return score;
   }
 }
 
-const burguer_ingredients = ["bread"];
+const burguer_ingredients = [
+  "white bread",
+  "lettuce",
+  "eggplant",
+  "spicy",
+  "cheddar",
+  "chicken",
+  "cheddar",
+  "beef",
+  "sesame bread",
+];
 console.log("The score is: " + Burguer.calculateScore(burguer_ingredients));
